1bf287f1595d9cecbe96af6aded3041928eaaac7
wombat <wombat@c36970fe-93d6-0310-93e0-db849999abed>
2001-06-01 17:35:35 +0000

    Fixed checkuser bug
    git-svn-id: svn+ssh://72.9.145.229/var/svn/open/trunk/hldfilter@32 c36970fe-93d6-0310-93e0-db849999abed

--- statsgen.pl
+++ statsgen.pl
@@ -10,7 +10,7 @@
 #      http://www.hld.ca/opensource/hldfilter
 #
 ###########################################################################
-# 	$rcs = ' $Id: statsgen.pl,v 3.0 2001/06/01 16:42:36 wombat Exp $ ' ;
+# 	$rcs = ' $Id: statsgen.pl,v 3.1 2001/06/01 17:35:32 wombat Exp $ ' ;
 ###########################################################################
 use strict;
 use Mail::Audit;		# this is for filtering mail
